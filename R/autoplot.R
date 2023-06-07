96da5ea2c314c4171504dbef0030c60db62f69b0
Philipp Fischer <36499405+PhilFischer@users.noreply.github.com>
2023-06-07 16:34:19 +0200

    Fix autoplot and documentation

--- R/autoplot.R
+++ R/autoplot.R
@@ -11,28 +11,41 @@ autoplot.FrameFit <- function(object, ...) {
 
   if(object$model_name == "stationary") return(autoplot.FrameFit.stationary(object, ...))
 
-  as.data.frame(object) %>%
+  data <- as.data.frame(object)
+  xlabel <- "Source"
+  if(is.null(object$vnames)) {
+    data <- data %>% rename(source = variable)
+    xlabel <- "Variable"
+  }
+  data %>%
     ggplot2::ggplot() +
     ggplot2::geom_ribbon(ggplot2::aes(x = t, ymin = `2.5%`, ymax = `97.5%`, fill = source), alpha = 0.4) +
     ggplot2::geom_line(ggplot2::aes(x = t, y = mean, col = source), linewidth = 1) +
     ggplot2::ylim(0, 1) +
     ggplot2::facet_grid(source ~ .) +
     ggplot2::xlab("Time") + ggplot2::ylab("Source Contribution and Fractionation Weight") +
-    ggplot2::labs(col = "Source", title = sprintf("Mean and 95%% Credible Interval (%s)", object$model_name)) +
+    ggplot2::labs(col = xlabel, title = sprintf("Mean and 95%% Credible Interval (%s)", object$model_name)) +
     ggplot2::guides(fill = "none") +
     ggplot2::theme_bw()
 }
 
 
 autoplot.FrameFit.stationary <- function(object, ...) {
-  as.data.frame(object) %>%
+  data <- as.data.frame(object)
+  xlabel <- "Source"
+  if(is.null(object$vnames)) {
+    data <- data %>% rename(source = variable)
+    xlabel <- "Variable"
+  }
+
+  data %>%
     ggplot2::ggplot() +
     ggplot2::geom_linerange(ggplot2::aes(x = source, ymin = `2.5%`, ymax = `97.5%`, col = source), linewidth = 0.8) +
     ggplot2::geom_linerange(ggplot2::aes(x = source, ymin = `25%`, ymax = `75%`, col = source), linewidth = 2) +
     ggplot2::geom_point(ggplot2::aes(x = source, y = `50%`, col = source), size = 5) +
     ggplot2::ylim(0, 1) +
-    ggplot2::xlab("Source") + ggplot2::ylab("Source Contribution and Fractionation Weight") +
-    ggplot2::labs(col = "Source", title = sprintf("Median, 50%% and 95%% Credible Intervals (%s)", object$model_name)) +
+    ggplot2::xlab(xlabel) + ggplot2::ylab("Source Contribution and Fractionation Weight") +
+    ggplot2::labs(title = sprintf("Median, 50%% and 95%% Credible Intervals (%s)", object$model_name)) +
     ggplot2::guides(col = "none") +
     ggplot2::theme_bw()
 }
